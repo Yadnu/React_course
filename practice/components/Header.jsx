d8e22942b8bd07d9ead5bf8e6d8140267466f68c
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2025-01-26 23:33:37 -0800

    Completed 1st challenge of HuXn Course

---
+++ practice/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+            <a href="home">Home</a>
+            <a href="about">About</a>
+            <a href="contact">Contact</a>
+        </nav>
+        <h1>
+        Welcome to My Website ! 
+    </h1>
+    
+    </div>
+    
+  )
+}
+
+export default Header
---
+++ practice/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h2>Main content</h2>
+        <p> This web site is under creation</p>
+    </main>
+  )
+}
+
+export default MainContent
